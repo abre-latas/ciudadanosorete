eaf15184354ac5c1d203cafc0eaec37d900c38a2
abre-latas <74990759+abre-latas@users.noreply.github.com>
2021-10-30 14:27:39 -0300

    Add files via upload

---
+++ javascript/envielamierda.js
@@ -0,0 +1,10 @@
+const botonenviar = document.getElementById('botonenviar');
+const direcciónEmail = document.getElementById('f1');
+if (f1.value === "") {
+    alert("Por favor, escribe tu correo electrónico");
+    f1.focus();
+    return false;
+  }
+  return true;
+
+
